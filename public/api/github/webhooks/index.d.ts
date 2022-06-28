888f7e1f044ba20c1cae3419a5baddd87028772a
Anthony Burzillo <burz@gitpoap.io>
2022-06-28 17:04:54 -0400

    GitHub doesn't send pull_request.merged events

--- public/api/github/webhooks/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-/// <reference types="pino-http" />
-declare const _default: import("http").RequestListener;
-export default _default;
--- public/bot.d.ts
+++
@@ -1,3 +0,0 @@
-import { Probot } from 'probot';
-declare const _default: (app: Probot) => void;
-export default _default;
