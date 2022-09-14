0ecaa0f67e06ea92c24c6646f1c7cfc391470d90
tyler415git <tyler@gitpoap.io>
2022-09-13 18:38:55 -0700

    feat: create claims on PR/issue comments

--- src/bot.ts
+++ src/bot.ts
@@ -14,6 +14,7 @@ Sentry.init({
 type BotClaimData = {
   id: number;
   gitPOAP: { id: number; poapEventId: number; threshold: number };
+  user: { githubHandle: string },
   name: string;
   imageUrl: string;
   description: string;
@@ -37,6 +38,31 @@ function generateComment(claims: BotClaimData[]): string {
 
   comment +=
     '\n\nHead on over to [GitPOAP.io](https://www.gitpoap.io) and connect your GitHub account to mint!';
+
+  return comment;
+}
+
+function generateIssueComment(claims: BotClaimData[]): string {
+  let qualifier: string;
+  if (claims.length > 1) {
+    qualifier = `some GitPOAPs`;
+  } else {
+    qualifier = `a GitPOAP`;
+  }
+
+  const receivers = claims.map(claim => claim.user.githubHandle);
+  const uniqueReceivers = Array.from(new Set(receivers));
+  const receiversTag = uniqueReceivers.reduce((acc, ele) => acc + `@${ele} `, "");
+  let comment = `Congrats, ${receiversTag}! You've earned ${qualifier} for your contribution!\n`;
+
+  for (const claim of claims) {
+    comment += `
+[**${claim.name}**](https://www.gitpoap.io/gp/${claim.gitPOAP.id}):
+<img alt="${claim.name} GitPOAP Badge" src="${claim.imageUrl}" height="200px">`;
+  }
+
+  comment +=
+    '\n\nHead on over to [GitPOAP.io](https://www.gitpoap.io) and connect your GitHub account to mint if you haven’t already!';
 
   return comment;
 }
@@ -104,17 +130,94 @@ export default (app: Probot) => {
     context.log.info(`Posted comment about new claims: ${result.data.html_url}`);
   });
 
-  // Useful for testing purposes:
-  //
-  //  app.on('pull_request.closed', async (context) => {
-  //    const pullRequestNum = context.payload.pull_request.number;
-  //    const issueComment = context.issue({
-  //      body: `Thanks for CLOSING PR #${pullRequestNum}!`,
-  //    });
-  //
-  //    context.log.info(context.payload.repository.owner);
-  //
-  //    context.log.info('PR HAS BEEN CLOSED');
-  //    await context.octokit.issues.createComment(issueComment);
-  //  });
+  app.on("issue_comment.created", async (context: Context<'issue_comment.created'>) => {
+    const repo = context.payload.repository.name;
+    const owner = context.payload.repository.owner.login;
+    const organization = context.payload.organization?.login ?? "";
+    const sender = context.payload.sender.login;
+    const comment = context.payload.comment.body;
+    const issueNumber = context.payload.issue.number;
+    const issueCreatorId = context.payload.issue.user.id;
+    const htmlURL = context.payload.issue.html_url;
+
+    const isPR = htmlURL?.includes(`/pull/${issueNumber}`);
+
+    // check if comment sender is repo owner
+    // TODO; uncomment after testing
+    // if(owner !== sender) {
+    //   return;
+    // }
+    // check if comment taggged gitpoap-bot
+    if(!comment.includes("@gitpoap-bot")) {
+      return;
+    }
+    // parse followed tagged contributors
+    const tagBody = comment.split("@gitpoap-bot ")[1];
+    const contributors = tagBody?.match(/@\w*/g)?.map(ele => ele.replace("@", "")).filter(ele => ele) ?? [];
+    const uniqueContributors =  Array.from(new Set(contributors));
+    // fetch github ids
+    const contributorGithubIds: number[] = [];
+    for(let contributor of uniqueContributors) {
+      const id: number = await fetch(`https://api.github.com/users/${contributor}`).then(res => res.json()).then(res => res.id);
+      contributorGithubIds.push(id)
+    }
+    // if there is no contributors tagged, we are going to awerad GitPOAP to PR/issue creator
+    if(contributorGithubIds.length === 0) {
+      contributorGithubIds.push(issueCreatorId);
+    }
+    // create claims for these contributors via API endpoint
+    const octokit = await app.auth(); // Not passing an id returns a JWT-authenticated client
+    const jwt = (await octokit.auth({ type: 'app' })) as { token: string };
+
+    const data = isPR ? {
+      pullRequest: {
+        organization,
+        repo,
+        pullRequestNumber: issueNumber,
+        contributorGithubIds,
+        wasEarnedByMention: true
+      }
+    } : {
+      issue: {
+        organization,
+        repo,
+        issueNumber,
+        contributorGithubIds,
+        wasEarnedByMention: false
+      }
+    }
+
+    const res = await fetch(`${process.env.API_URL}/claims/gitpoap-bot/create`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt.token}`,
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (res.status !== 200) {
+      context.log.error(`An issue occurred (response code: ${res.status}): ${await res.text()}`);
+      return;
+    }
+
+    // create a comment to show info about gitpoap
+    const response = await res.json();
+
+    if (response.newClaims.length === 0) {
+      context.log.info('No new claims were created by this PR');
+      return;
+    }
+
+    context.log.info(`${response.newClaims.length} new Claims were created by this PR`);
+
+    const issueComment = context.issue({
+      body: generateIssueComment(response.newClaims),
+    });
+
+    const result = await context.octokit.issues.createComment(issueComment);
+
+    context.log.info(`Posted comment about new claims: ${result.data.html_url}`);
+  });
 };
