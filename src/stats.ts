1e10629a080037f36b9bb1ff40c4eb0949c56101
tyler415git <110413633+tyler415git@users.noreply.github.com>
2022-09-14 09:40:29 -0700

    Revert "feat: create claims on PR/issue comments"
    This reverts commit c6211d2dbb8a41faaad188861d896435c1970b0a.

--- src/stats.ts
+++ src/stats.ts
@@ -4,15 +4,15 @@ const PER_PAGE = 100;
 
 function getApp() {
   return new App({
-    appId: process.env.APP_ID ?? "",
-    privateKey: process.env.PRIVATE_KEY ?? "",
+    appId: process.env.APP_ID,
+    privateKey: process.env.PRIVATE_KEY,
   });
 }
 
 export async function getBotInstalls() {
   const app = getApp();
 
-  let results: any = [];
+  let results = [];
 
   let page = 1;
   for (let count = PER_PAGE; count === PER_PAGE; ++page) {
@@ -21,7 +21,7 @@ export async function getBotInstalls() {
       per_page: PER_PAGE,
     });
     count = installations.length;
-    results = results.concat(installations.map(i => i?.account?.html_url));
+    results = results.concat(installations.map(i => i.account.html_url));
   }
 
   return results;
