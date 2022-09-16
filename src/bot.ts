b161ff48cc0e267bace460b98f5439ec381bc45b
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-09-16 16:15:16 +0200

    lint bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,7 @@
 import { Context, Probot } from 'probot';
 import { fetch } from 'cross-fetch';
 import * as Sentry from '@sentry/node';
-import {generateComment, generateIssueComment} from "./comments";
+import { generateComment, generateIssueComment } from './comments';
 
 /* @probot/pino automatically picks up SENTRY_DSN from .env */
 Sentry.init({
@@ -22,7 +22,7 @@ export default (app: Probot) => {
     const owner = context.payload.repository.owner.login;
     const pullRequestNumber = context.payload.number;
     const senderId = context.payload.pull_request.user.id;
-    const organization = context.payload.organization?.login ?? "";
+    const organization = context.payload.organization?.login ?? '';
 
     context.log.info(
       `Handling newly merged PR: https://github.com/${owner}/${repo}/${pullRequestNumber}`,
@@ -31,7 +31,7 @@ export default (app: Probot) => {
     // Skip claims creation API request if the creator of the PR is a bot
     if (context.payload.pull_request.user.type === 'Bot') {
       context.log.info(
-        `Skipping creating claims for PR made by bot "${context.payload.pull_request.user.login}"`
+        `Skipping creating claims for PR made by bot "${context.payload.pull_request.user.login}"`,
       );
       return;
     }
@@ -52,8 +52,8 @@ export default (app: Probot) => {
           repo,
           pullRequestNumber: pullRequestNumber,
           contributorGithubIds: [senderId],
-          wasEarnedByMention: false
-        }
+          wasEarnedByMention: false,
+        },
       }),
     });
 
@@ -80,10 +80,10 @@ export default (app: Probot) => {
     context.log.info(`Posted comment about new claims: ${result.data.html_url}`);
   });
 
-  app.on("issue_comment.created", async (context: Context<'issue_comment.created'>) => {
+  app.on('issue_comment.created', async (context: Context<'issue_comment.created'>) => {
     const repo = context.payload.repository.name;
     const owner = context.payload.repository.owner.login;
-    const organization = context.payload.organization?.login ?? "";
+    const organization = context.payload.organization?.login ?? '';
     const sender = context.payload.sender.login;
     const comment = context.payload.comment.body;
     const issueNumber = context.payload.issue.number;
@@ -93,55 +93,61 @@ export default (app: Probot) => {
     const isPR = htmlURL?.includes(`/pull/${issueNumber}`);
 
     // check if comment sender is repo owner
-    if(owner !== sender) {
+    if (owner !== sender) {
       return;
     }
     // check if comment taggged gitpoap-bot
-    if(!comment.includes("@gitpoap-bot")) {
+    if (!comment.includes('@gitpoap-bot')) {
       context.log.info(`Sender didn't tag @gitpoap-bot in this comment`);
       return;
     }
     // parse followed tagged contributors
-    const tagBody = comment.split("@gitpoap-bot ")[1];
-    const contributors = tagBody?.match(/@\w*/g)?.map(contributor => contributor.replace("@", "")).filter(contributor => contributor) ?? [];
-    const uniqueContributors =  Array.from(new Set(contributors));
+    const tagBody = comment.split('@gitpoap-bot ')[1];
+    const contributors =
+      tagBody
+        ?.match(/@\w*/g)
+        ?.map((contributor) => contributor.replace('@', ''))
+        .filter((contributor) => contributor) ?? [];
+    const uniqueContributors = Array.from(new Set(contributors));
     // fetch github ids
     const contributorGithubIds: number[] = [];
-    for(let contributor of uniqueContributors) {
+    for (let contributor of uniqueContributors) {
       const res = await context.octokit.users.getByUsername({
-        username: contributor
+        username: contributor,
       });
       const user = res.data;
 
-      if(user && user.id) {
-        contributorGithubIds.push(user?.id)
+      if (user && user.id) {
+        contributorGithubIds.push(user?.id);
       }
     }
     // if there are no contributors tagged, we award GitPOAP(s) to the PR/issue creator
-    if(contributorGithubIds.length === 0) {
+    if (contributorGithubIds.length === 0) {
       contributorGithubIds.push(issueCreatorId);
     }
     // create claims for these contributors via API endpoint
     const octokit = await app.auth(); // Not passing an id returns a JWT-authenticated client
     const jwt = (await octokit.auth({ type: 'app' })) as { token: string };
 
-    const data = isPR ? {
-      pullRequest: {
-        organization,
-        repo,
-        pullRequestNumber: issueNumber,
-        contributorGithubIds,
-        wasEarnedByMention: true
-      }
-    } : {
-      issue: {
-        organization,
-        repo,
-        issueNumber,
-        contributorGithubIds,
-        wasEarnedByMention: true
-      }
-    }
+    const data = isPR
+      ? {
+          pullRequest: {
+            organization,
+            repo,
+            pullRequestNumber: issueNumber,
+            contributorGithubIds,
+            wasEarnedByMention: true,
+          },
+        }
+      : {
+          issue: {
+            organization,
+            repo,
+            issueNumber,
+            contributorGithubIds,
+            wasEarnedByMention: true,
+          },
+        };
 
     const res = await fetch(`${process.env.API_URL}/claims/gitpoap-bot/create`, {
       method: 'POST',
