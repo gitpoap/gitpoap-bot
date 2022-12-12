4826c126900f4a7e01d53c8440e9a53580b7c940
ponyjackal <ponyjackal@Ponys-MacBook-Pro.local>
2022-12-12 12:08:34 -0800

    feat: add slack notification

---
+++ src/slack.ts
@@ -0,0 +1,51 @@
+import { WebClient } from '@slack/web-api';
+
+const slackClient = new WebClient(process.env.SLACK_TOKEN);
+
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+type SlackOrgs = 'gitpoap';
+
+type SlackChannels = {
+  [org in SlackOrgs]: Record<string, string>;
+};
+
+const CHANNELS: SlackChannels = {
+  gitpoap: {
+    alerts: 'C049AGT4BHN',
+    devAlerts: 'C049XUFTQ5B',
+  },
+};
+
+const sendSlackMessage = async (message: string) => {
+  const channel = IS_PROD ? CHANNELS.gitpoap.alerts : CHANNELS.gitpoap.devAlerts;
+  try {
+    console.debug(`Sending slack message to channel: ${channel}`);
+    await slackClient.chat.postMessage({
+      channel,
+      text: message,
+    });
+  } catch (e) {
+    console.error(`${e}`);
+  }
+};
+
+export const sendBotMentionedMessage = (
+  comment: string,
+  sender: string,
+  htmlURL: string,
+  repo: string,
+) => {
+  const msg = `:robot_face: @gitpoap-bot was mentioned in <${htmlURL}|${repo}> repository.
+    * Sender GitHub handle: <https://github.com/${sender}|@${sender}>
+    * Comment: ${comment}`;
+
+  sendSlackMessage(msg);
+};
+
+export const sendGitPOAPIssueMessage = (comment: string, htmlURL: string, repo: string) => {
+  const msg = `:robot_face: @gitpoap-bot issued GitPOAPs to contributors in <${htmlURL}|${repo}> repository.
+      * Comments: ${comment}`;
+
+  sendSlackMessage(msg);
+};
